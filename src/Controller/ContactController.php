ce57ad2ac7245de50d484d6ce9050f931969b631
Thusy <vr.master99@gmail.com>
2019-04-11 17:29:00 +0200

    contact list en cours de creation

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,15 +3,22 @@
 namespace App\Controller;
 
 use App\Entity\Contact;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class ContactController
+ * @package App\Controller
+ *
+ * @Route("/contact")
+ */
 class ContactController extends AbstractController
 {
     /**
      * @Route("/{id}")
      */
-    public function index(Contact $contacts)
+    public function index(User $user)
     {
         $repository = $this->getDoctrine()->getRepository(Contact::class);
         $contacts = $repository->findBy([], ['name' => 'ASC']);
