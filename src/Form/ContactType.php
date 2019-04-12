53de46c1fd7a2a03bea8c50575ac93caab26b297
Thusy <vr.master99@gmail.com>
2019-04-12 17:49:15 +0200

    fin journee

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -3,7 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Contact;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +14,25 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('email')
+            ->add('nom',
+                TextType::class,
+                [
+                    'label' => 'Nom'
+                ]
+
+            )
+            ->add('prenom',
+                TextType::class,
+                [
+                    'label' => 'Prénom'
+                ]
+            )
+            ->add('email',
+                EmailType::class,
+                [
+                    'label' => 'Email'
+                ]
+                )
             ->add('dateDeNaissance')
             ->add('telephone')
             ->add('profession')
