ca888ed36a0c63ad7a0b19fb39d0ffd76a6198e4
DjangoBang <bimboumbadaboum@hotmail.com>
2019-04-19 13:50:20 +0200

    js, bg anime, page search results, cosmetique globale

--- public/js/functions.js
+++ public/js/functions.js
@@ -30,3 +30,73 @@ getMessage();
     //setInterval(getMessage, 2000);
 
 });
+
+
+
+
+// ------------ particlesJS --------------
+
+particlesJS
+
+("particles-js", {
+    "particles": {
+        "number": {"value": 80, "density": {"enable": true, "value_area": 800}},
+        "color": {"value": "#ffffff"},
+        "shape": {
+            "type": "circle",
+            "stroke": {"width": 0, "color": "#000000"},
+            "polygon": {"nb_sides": 5},
+            // "image": {"src": "img/github.svg", "width": 100, "height": 100}
+        },
+        "opacity": {
+            "value": 0.5,
+            "random": false,
+            "anim": {"enable": false, "speed": 1, "opacity_min": 0.1, "sync": false}
+        },
+        "size": {"value": 3, "random": true, "anim": {"enable": false, "speed": 40, "size_min": 0.1, "sync": false}},
+        "line_linked": {"enable": true, "distance": 150, "color": "#ffffff", "opacity": 0.4, "width": 1},
+        "move": {
+            "enable": true,
+            "speed": 6,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {"enable": false, "rotateX": 600, "rotateY": 1200}
+        },
+    },
+
+    "interactivity": {
+        "events": {
+            "onhover": {
+                "enable": false,
+            },
+            "onclick": {
+                "enable": false,
+            },
+        },
+
+    },
+
+    "retina_detect": true
+});
+
+
+// // ------------ TOGGLE --------------
+//
+//CSS
+// <input type="checkbox" id="menu34" />
+//     <label for="menu34">
+//JS
+// var inputs = document.getElementsByTagName("toggle-cross"),
+//     labels = document.getElementsByTagName("toggle-cross");
+//
+//
+// // Flashes menus on load
+// document.addEventListener("DOMContentLoaded", function(event) {
+//     check(true);
+// });
+
+
+
